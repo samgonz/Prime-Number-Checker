074f654613e027424f6e96365ed5de9f4bf72b1a
samgonz <doomteam1@gmail.com>
2022-09-29 23:07:22 -0500

    added a title bar splash

--- primeNumberChecker.py
+++ primeNumberChecker.py
@@ -1,1 +1,7 @@
+import checkIfPrime
+from welcomeMessage import welcome_message
 
+welcome_message()
+check_if_prime = int(input('\nWhat number would you like to check to see if it is a prime number?\n'))
+
+print(f'After checking, {check_if_prime}, {checkIfPrime.is_prime(check_if_prime)}.')
